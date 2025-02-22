4c064b50297437f6c78e4dbd5a66a2b1d9819ca1
Wojciech Kazmierski <wkazmierski6@gmail.com>
2025-02-23 00:49:46 +0100

    Added functionality for user to select length of the password

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import random
 import string
+
+def generate_password():
+    while True:
+
+        length = input("Insert length of password: ")
+        if not length.isdigit():
+            print("You can't type letters in this field! Please try again!")
+        else:
+            length = int(length)
+            print("Success")
+        
+
+        print("1. Special Characters")
+        print("2. Numbers")
+        print("3. Uppercase Letters")
+        print("4. Lowercase Letters")
+        requirements = input("Select which requirements you want to password contain (sperate them by commas!):  ")
+
+generate_password()
