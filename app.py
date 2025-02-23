744c3d1edcc115eac50a306c954471a75831cdb0
Wojciech Kazmierski <wkazmierski6@gmail.com>
2025-02-23 01:07:28 +0100

    Fixed other errors

--- app.py
+++ app.py
@@ -3,31 +3,35 @@ import string
 
 def generate_password():
     while True:
-
         length = input("Insert length of password: ")
         if not length.isdigit():
             print("You can't type letters in this field! Please try again!")
-        else:
-            length = int(length)
-            print("Success")
-        
+            continue
+        length = int(length)
 
         print("1. Special Characters")
         print("2. Numbers")
         print("3. Uppercase Letters")
         print("4. Lowercase Letters")
-        requirements = input("Select which requirements you want to password contain (sperate them by commas!):  ")
+        requirements = input("Select which requirements you want the password to contain (separate them by commas!):  ")
 
         characters = ""
+
         if "1" in requirements:
             characters += string.punctuation
         if "2" in requirements:
             characters += string.digits
         if "3" in requirements:
-            characters +=  string.ascii_uppercase
+            characters += string.ascii_uppercase
         if "4" in requirements:
             characters += string.ascii_lowercase
 
+        if not characters:
+            print("You must select at least one option!")
+            continue
+
         password = ''.join(random.choice(characters) for _ in range(length))
+        print(f"Generated password: {password}")
+        return password
 
 generate_password()
